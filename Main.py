303a1e2be6f83cbac43a0eb25539115ef8a6d750
LordVenter <60590862+lordventer@users.noreply.github.com>
2020-02-04 23:57:42 +1000

    removed old to_catagorical

--- Main.py
+++ Main.py
@@ -16,22 +16,6 @@ train_loader = DataLoader(datasets.MNIST('../data', train=True, download=True, t
 test_loader = DataLoader(datasets.MNIST('../data', train=False, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])), batch_size=batch_size, shuffle=True)
 
 
-def to_categorical(y, num_classes=None, dtype='float32'):
-    y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
-    output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
-    return categorical
-
-
 def to_categorical(tensor:Tensor, num_classes=None):
     newtensor = []
     for t in tensor:
